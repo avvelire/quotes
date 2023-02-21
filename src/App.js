723af4f803ecec7a45c2823b6fa5483e3fc0fef0
dAndrVi <vasylkivandrej@gmail.com>
2023-02-21 15:35:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+
 function App() {
+  const [data, setData] = useState([])
+  const [object, setObject] = useState(null)
+
+  useEffect(() => {
+
+    fetch(URL)
+    .then(response => response.json())
+    .then(response => setData(response.quotes))
+
+  }, [])
+
+
+  const generateQuote = () => {
+    const dataIndex = Math.floor(Math.random() * data.length)
+    setObject(data[dataIndex])
+  }
+
+
+  const resault = object
+  ?
+    <div>
+      <h1>{object.quote}</h1>
+      {object.author === "Unknown" ? <h2>"The author is unknown"</h2> : <h2>"{object.author}"</h2>}
+    </div>
+  : <h1 className='start'>Press the button</h1>
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='child'>
+          {resault}
+          <div>
+            <button onClick={generateQuote}>Get Quote</button>
+          </div>
+        </div>
+      </div>
   );
 }
 
